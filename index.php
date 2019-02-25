d98364ea494eb1653c5b75b68c4715c24c8d2d32
codersaiful3200 <engsaifulislam559@gmail.com>
2019-02-25 19:02:51 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+    <title>Calculator</title>
+</head>
+<body>
+
+<div class="page">
+
+    <div class="header"><b>Resistation From</b></div>
+    <div class="form">
+        <label for="First-name">First Name</label>
+        <input type="text" placeholder="Enter Your First Name" name="Firstname">
+        <br/>
+
+        <label for="First-name">Last Name</label>
+        <input type="text" placeholder="Enter Your Last Name" name="Lastname">
+        <br/>
+
+        <label for="First-name">User Name</label>
+        <input type="text" placeholder="Enter Your User Name" name="Username">
+        <br/>
+
+        <label for="First-name">Email</label>
+        <input type="email" placeholder="Enter Your email" name="Email">
+        <br/>
+
+        <label for="Password">Password</label>
+        <input type="password" placeholder="Enter Your Password" name="password">
+        <br/>
+
+       <a href="register.php"><button type="submit" class="bt1">Resister</button></a>
+        <a href="login.php" class="bt2">Login</a>
+
+    </div>
+
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+
+echo  "Wellcome";
+echo "</br>";
+
+echo "Youe Resistation successfull";
+echo "</br>";
+echo "your name is : saiful islam saif";
+
+?>
